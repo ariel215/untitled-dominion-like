eef568ef875392edfeb381f52bf92aebd1cdf864
Leon Davis <lad258@cornell.edu>
2020-06-24 22:38:16 -0500

    CardPool also responsible for holding card prefabs, once they exist

--- Assets/Code/Objects/CardPool.cs
+++ Assets/Code/Objects/CardPool.cs
@@ -6,17 +6,30 @@ using Cards;
 public class CardPool
 {
 
-    private List<CardData> cards;
+    private List<CardData> Cards;
     Player Player;
+    Dictionary<string, GameObject> cardPrefabs;
 
     public static CardPool Load(string path, Player player)
     {
         throw new NotImplementedException();
     }
 
-    private CardPool(Player p)
+    public CardPool(Player p, IEnumerable<CardData> cardsIn)
     {
         Player = p;
+        Cards = new List<CardData>(cardsIn);
+        Cards.Sort((x, y) => x.MinDamage.CompareTo(y.MinDamage));
+    }
+
+    public void AddCards(IEnumerable<CardData> newCards)
+    {
+        foreach(var card in newCards)
+        {
+            Cards.Add(card);
+        }
+        Cards.Sort((x, y) => x.MinDamage.CompareTo(y.MinDamage));
+
     }
 
     public CardData Fixed()
@@ -28,6 +41,27 @@ public class CardPool
     {
         throw new NotImplementedException();
     }
+
+    void LoadPrefabs()
+    {
+        foreach(var card in Cards)
+        {
+            cardPrefabs[card.Name] = Resources.Load<GameObject>(card.Name);
+        }
+    }
+
+    public GameObject GetObject(CardData card)
+    {
+        return GameObject.Instantiate(cardPrefabs[card.Name]);
+    }
+
+    public GameObject GetObject(string name)
+    {
+        return GameObject.Instantiate(cardPrefabs[name]);
+    }
+
+
+
 
 
 }
